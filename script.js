b43b0c75de8346478a8b114c2e3c16a03e3b56ce
Your Name <c.fear.907@gmail.com>
2025-03-04 21:04:08 +0000

    Reviews Added Index Good More to Do...

--- script.js
+++ script.js
@@ -286,4 +286,35 @@ backToTopButton.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
-s
+
+
+
+//reviewspage slider 
+
+document.addEventListener('DOMContentLoaded', function() {
+    new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        }
+    });
+});
